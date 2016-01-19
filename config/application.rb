07086dd43562636aafd7ed461a5196e3c38e1459
Gorchel <kasatka567@gmail.com>
2016-01-19 20:36:30 +0300

    next

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module Video
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
+    config.assets.compile = true
+    config.serve_static_assets = true
   end
 end
